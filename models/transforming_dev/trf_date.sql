e50773d4d91291d1c9aeb2714fa1f618c93d740e
Ajinkya Sawant <ajinkya.ashoksawant@cognizant.com>
2025-11-17 06:41:24 +0000

    commit trans and lkp schema

--- models/transforming_dev/trf_date.sql
+++ models/transforming_dev/trf_date.sql
@@ -1,4 +1,4 @@
-{{ config(materialized = 'table', schema = 'transforming_dev') }}
+{{ config(materialized = 'table', schema = env_var('DBT_TRANSSCHEMA_NAME','TRANSFORMING_DEV')) }}
 
 {% set min_order_date = get_min_orderdate() %}
 {% set max_order_date = get_max_orderdate() %}
